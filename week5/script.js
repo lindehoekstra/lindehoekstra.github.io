25362a1f6637520ff7210c8f49af597c5568cc55
L1nd3H <l.hoekstra@student.tue.nl>
2025-04-03 15:28:22 +1100

    background color changes

---
+++ week5/script.js
@@ -0,0 +1,33 @@
+console.log("Hello");
+console.log("How are you");
+
+let myName = "Linde";
+console.log("Hello", myName);
+let id = 1234;
+console.log("my id is", id);
+// let myCity = prompt("where do you live?");
+// console.log("I live in", myCity);
+// promt is ugly, so you wont use it much, there are better ways to get interaction
+
+let isItSunday = false;
+const OART1013 = true;
+console.log(isItSunday);
+
+const myStudentrecord = {
+  name: "Linde",
+  id: 1234,
+  homeTown: "Melb",
+  isLocal: false,
+};
+
+console.log(myStudentrecord);
+console.log(myStudentrecord.homeTown);
+
+z = "37";
+y = 10;
+m = z + y;
+console.log(m);
+console.log(parseInt(z) + 10);
+
+let students = ["Linde", "Lucy", "Jim", "Mike"];
+console.log(students[0]);
