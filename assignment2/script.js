f3af972f89052f03d29fc59f369758f50b918c81
L1nd3H <l.hoekstra@student.tue.nl>
2025-04-26 14:54:39 +1000

    bijna werkende image wissel

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,23 +1,23 @@
-const video = document.querySelector("#custom-video-player");
-const playPauseBtn = document.querySelector("#play-pause-btn");
-const playPauseImg = document.querySelector("#play-pause-img");
-const progressBar = document.querySelector("#progress-bar-fill");
-video.removeAttribute("controls");
-// playPauseBtn.addEventListener("click", togglePlayPause);
-video.addEventListener("timeupdate", updateProgressBar);
-function togglePlayPause() {
-  if (video.paused || video.ended) {
-    video.play();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
-  } else {
-    video.pause();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
-  }
-}
-function updateProgressBar() {
-  const value = (video.currentTime / video.duration) * 100;
-  progressBar.style.width = value + "%";
-}
+// const video = document.querySelector("#custom-video-player");
+// const playPauseBtn = document.querySelector("#play-pause-btn");
+// const playPauseImg = document.querySelector("#play-pause-img");
+// const progressBar = document.querySelector("#progress-bar-fill");
+// video.removeAttribute("controls");
+// // playPauseBtn.addEventListener("click", togglePlayPause);
+// video.addEventListener("timeupdate", updateProgressBar);
+// function togglePlayPause() {
+//   if (video.paused || video.ended) {
+//     video.play();
+//     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+//   } else {
+//     video.pause();
+//     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+//   }
+// }
+// function updateProgressBar() {
+//   const value = (video.currentTime / video.duration) * 100;
+//   progressBar.style.width = value + "%";
+// }
 // Add other functionalities here
 
 // Once a song button is clicked the corresponding audio file needs to start playing
@@ -49,76 +49,23 @@ console.log(button3);
 const button4 = document.querySelector(".button4");
 console.log(button4);
 
+// pictures for each track
+let track1pic = document.querySelector("#track1picture");
+console.log(track1pic);
+
+let track2pic = document.querySelector("#track2picture");
+console.log(track2pic);
+
+let track3pic = document.querySelector("#track3picture");
+console.log(track3pic);
+
+let track4pic = document.querySelector("#track4picture");
+console.log(track4pic);
+
+let beginPic = document.querySelector("#begin-pic");
+console.log(beginPic);
+
 // now we connect the 2 together, once the button is clicked the track starts
-// button1.addEventListener(
-//   "click",
-//   playTrack1,
-//   pauseTrack2,
-//   pauseTrack3,
-//   pauseTrack4
-// );
-
-// function playTrack1() {
-//   track1.play();
-// }
-
-// // other tracks
-// button2.addEventListener(
-//   "click",
-//   playTrack2,
-//   pauseTrack1,
-//   pauseTrack3,
-//   pauseTrack4
-// );
-
-// function playTrack2() {
-//   track2.play();
-// }
-
-// button3.addEventListener(
-//   "click",
-//   playTrack3,
-//   pauseTrack1,
-//   pauseTrack2,
-//   pauseTrack4
-// );
-
-// function playTrack3() {
-//   track3.play();
-// }
-
-// button4.addEventListener(
-//   "click",
-//   playTrack4,
-//   pauseTrack1,
-//   pauseTrack2,
-//   pauseTrack3
-// );
-
-// function playTrack4() {
-//   track4.play();
-// }
-
-// pause the other tracks
-// we dont want the other tracks to keep playing once we click on a certain track,
-// so we have to add a pause for the other tracks
-
-// function pauseTrack1() {
-//   track1.pause();
-// }
-
-// function pauseTrack2() {
-//   track2.pause();
-// }
-
-// function pauseTrack3() {
-//   track3.pause();
-// }
-
-// function pauseTrack4() {
-//   track4.pause();
-// }
-
 // https://www.reddit.com/r/learnjavascript/comments/mageyv/music_player_help_how_to_pause_and_play_multiple/
 // https://www.w3schools.com/js/js_arrays.asp
 // https://www.w3schools.com/jsref/jsref_foreach.asp
@@ -128,6 +75,10 @@ console.log(album);
 const buttons = [button1, button2, button3, button4];
 console.log(buttons);
 
+let pictures = [track1pic, track2pic, track3pic, track4pic];
+console.log(pictures);
+
+// click on a track and play the coresponding audio file, make sure the others are paused
 buttons.forEach(function (button, index) {
   button.addEventListener("click", function () {
     album.forEach(function (track, i) {
@@ -140,6 +91,19 @@ buttons.forEach(function (button, index) {
       } else {
         track.pause();
         track.currentTime = 0;
+      }
+    });
+  });
+});
+
+// put the right image in the placeholder image that it starts with
+buttons.forEach(function (button, index) {
+  button.addEventListener("click", function () {
+    pictures.forEach(function (img, i) {
+      if (i === index) {
+        img.classList.add("active");
+      } else {
+        img.classList.remove("active");
       }
     });
   });
