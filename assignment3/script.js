cdcf5cd7b72bea153844cdc7e78bd54799e97340
L1nd3H <l.hoekstra@student.tue.nl>
2025-05-29 16:14:44 +1000

    flippable paper gemaakt

---
+++ assignment3/script.js
@@ -0,0 +1,10 @@
+// for the orgimani I chose a simple tutorial with minimal steps
+//  the tutorial I chose to replicate with my application is the following video: https://www.youtube.com/watch?v=PlebdXZRqP0
+// It is a tutorial for a paper heart
+
+const paper = document.querySelector(".paper");
+console.log(paper);
+
+paper.addEventListener("click", function () {
+  paper.classList.toggle("flip");
+});
