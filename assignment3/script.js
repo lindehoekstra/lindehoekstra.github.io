7fb333cd7b5e6f8c4b5d5aa285bdadeba6fac78f
L1nd3H <l.hoekstra@student.tue.nl>
2025-06-04 13:41:31 +1000

    buttons toegevoegd in de footer en voor de stappen

--- assignment3/script.js
+++ assignment3/script.js
@@ -2,9 +2,12 @@
 //  the tutorial I chose to replicate with my application is the following video: https://www.youtube.com/watch?v=PlebdXZRqP0
 // It is a tutorial for a paper heart
 
+// flip the paper to front and back
 const paper = document.querySelector(".paper");
 console.log(paper);
 
 paper.addEventListener("click", function () {
   paper.classList.toggle("flip");
 });
+
+//
