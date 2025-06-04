400c217ebe874fae8a720d142fd555450ea1ab5c
L1nd3H <l.hoekstra@student.tue.nl>
2025-06-04 13:59:31 +1000

    start aan js

--- assignment3/script.js
+++ assignment3/script.js
@@ -10,4 +10,41 @@ paper.addEventListener("click", function () {
   paper.classList.toggle("flip");
 });
 
-//
+// buttons from the individual steps
+const step1 = document.querySelector("#circle1");
+console.log(step1);
+
+const step2 = document.querySelector("#circle2");
+console.log(step2);
+
+const step3 = document.querySelector("#circle3");
+console.log(step3);
+
+const step4 = document.querySelector("#circle4");
+console.log(step4);
+
+const step5 = document.querySelector("#circle5");
+console.log(step5);
+
+const step6 = document.querySelector("#circle6");
+console.log(step6);
+
+const step7 = document.querySelector("#circle7");
+console.log(step7);
+
+// put all the step buttons in an array
+const steps = [step1, step2, step3, step4, step5, step6, step7];
+console.log(steps);
+
+// Buttons from the footer
+const prevButton = document.querySelector("#previous");
+console.log(prevButton);
+
+const nextButton = document.querySelector("#next");
+console.log(nextButton);
+
+const resetButton = document.querySelector("#reset");
+console.log(resetButton);
+
+// use a for loop to go through all the step buttons
+for (let i = 0; i < steps.length; i++) {}
